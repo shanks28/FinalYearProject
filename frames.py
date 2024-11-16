6a019117932777276e728b2c8b1d1adb16f58f70
Saul Goodman <1rn21cs016.akash@gmail.com>
2024-11-16 14:08:45 +0530

    foo

---
+++ frames.py
@@ -0,0 +1,15 @@
+import cv2
+import os
+def extract_frames(url_path,output_dir):
+    os.makedirs(output_dir, exist_ok=True)
+    frame_count=0
+    cap=cv2.VideoCapture(url_path)
+    while cap.isOpened() and frame_count<10:
+        ret,frame=cap.read()
+        if not ret:
+            break
+        frame_name=f"{frame_count}.png"
+        cv2.imwrite(os.path.join(output_dir, frame_name), frame)
+        frame_count+=1
+    cap.release()
+extract_frames("C:/Users/BRIDGES/Downloads/Video1.mp4","output")
